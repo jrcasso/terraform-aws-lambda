e03b4a61cd6b787f40a4c7d51d5f6f29b281261e
Justin Casso <justincasso1@gmail.com>
2023-11-23 10:06:54 -0500

    Modify rebuild trigger to use sha256 of file contents instead of timestamp

--- package.tf
+++ package.tf
@@ -61,7 +61,7 @@ resource "null_resource" "archive" {
 
   triggers = {
     filename  = data.external.archive_prepare[0].result.filename
-    timestamp = data.external.archive_prepare[0].result.timestamp
+    sha256content = sha256(data.external.archive_prepare[0].result.content)
   }
 
   provisioner "local-exec" {
